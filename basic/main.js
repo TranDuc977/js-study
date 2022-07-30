033c0817b252c276099246fc75488ce8da859f7f
tranduc977 <tranducc97@gmail.com>
2022-07-30 23:17:52 +0700

    first commit

---
+++ basic/main.js
@@ -0,0 +1,24 @@
+// let x = "hello1";
+// let y = "world";
+// let z = x;
+// x = y;
+// y = z;
+// console.log(x);
+// console.log(y);
+
+// const COLOR_ORANGE = "#FF0000";
+// const COLOR_GREEN = "#00FF00";
+// console.log(COLOR_ORANGE);
+// console.log(COLOR_GREEN);
+// const currentColor = COLOR_ORANGE;
+// console.log(currentColor);
+
+//Hiển thị tuổi qua thông báo alert
+// const age = prompt('How old are you?', 18);
+// alert(`You are ${age} years old`);
+
+let a = 1;
+let x = ((a = a + 1), a + 4);
+console.log(a);
+console.log(x);
+console.log(a);
